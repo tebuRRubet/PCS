6d137e15bd455b5f5ade4f0d8d680653b9b08ca0
tebuRRubet <rubet.jurgens@student.uva.nl>
2025-01-18 17:40:11 +0100

    Update lbm.py

--- lbm.py
+++ lbm.py
@@ -3,57 +3,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-output_dir = "frames2"
-os.makedirs(output_dir, exist_ok=True)
-
-
 def distance(x1, y1, x2, y2):
     return np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 
 def main():
-    Nx = 400
-    Ny = 100
+    plot_every = 1
+    Nx, Ny = 400, 100
     tau = 0.53
-    Nt = 5000
-    plot_every = 100
-    save = False
+    Nt = 3000
 
-    # Lattice speeds and weights.
     NL = 9
     cxs = np.array([0, 0, 1, 1, 1, 0, -1, -1, -1])
     cys = np.array([0, 1, 1, 0, -1, -1, -1, 0, 1])
-    weights = np.array([4 / 9, 1 / 9, 1 / 36, 1 / 9, 1 / 36, 1 / 9, 1 / 36,
-                        1 / 9, 1 / 36])
+    weights = np.array([4/9, 1/9, 1/36, 1/9, 1/36, 1/9, 1/36, 1/9, 1/36])
 
-    # Initial conditions.
     F = np.ones((Ny, Nx, NL)) + 0.01 * np.random.randn(Ny, Nx, NL)
     F[:, :, 3] = 2.3
 
     cylinder = np.full((Ny, Nx), False)
-
     for y in range(Ny):
         for x in range(Nx):
-            if distance(Nx // 4, Ny // 2, x, y) < 13:
-                r = x**2 + y**2
-                a = int(x * (1 + 1 / r))
-                b = int(y * (1 - 1 / r))
-                cylinder[b][a] = True
+            if distance(Nx / 4, Ny / 2, x, y) < 13:
+                cylinder[y][x] = True
 
-    # Main loop.
     for it in range(Nt):
-
-        F[:, -1, [6, 7, 8]] = F[:, -2, [6, 7, 8]]
-        F[:, 0, [2, 3, 4]] = F[:, 1, [2, 3, 4]]
-
+        print(it)
         for i, cx, cy in zip(range(NL), cxs, cys):
             F[:, :, i] = np.roll(F[:, :, i], cx, axis=1)
             F[:, :, i] = np.roll(F[:, :, i], cy, axis=0)
-
         bndryF = F[cylinder, :]
         bndryF = bndryF[:, [0, 5, 6, 7, 8, 1, 2, 3, 4]]
 
-        # Fluid variables.
         rho = np.sum(F, 2)
         ux = np.sum(F * cxs, 2) / rho
         uy = np.sum(F * cys, 2) / rho
@@ -62,32 +43,17 @@ def main():
         ux[cylinder] = 0
         uy[cylinder] = 0
 
-        # Collision
         Feq = np.zeros(F.shape)
         for i, cx, cy, w in zip(range(NL), cxs, cys, weights):
-            uu = ux**2 + uy**2
-            vu = cx * ux + cy * uy
             Feq[:, :, i] = rho * w * (
-                1 + 3 * vu + (9 / 2) * vu**2 - (3 / 2) * uu
+                1 + 3 * (cx * ux + cy * uy) + 9 * (cx * ux + cy * uy) ** 2 / 2 - 3 * (ux ** 2 + uy ** 2) / 2
             )
+            F = F + -(1/tau) * (F - Feq)
 
-        F -= (F - Feq) / tau
-
-        if it % plot_every == 0:
-            dfydx = ux[2:, 1:-1] - ux[0:-2, 1:-1]
-            dfxdy = uy[1:-1, 2:] - uy[1:-1, 0:-2]
-            curl = dfydx - dfxdy
-
-            plt.imshow(curl, cmap="bwr")
-            # plt.imshow(np.sqrt(uu))
-
-            if save:
-                frame_path = os.path.join(output_dir, f"frame_{it:04d}.png")
-                plt.savefig(frame_path)
-
+        if (it % plot_every == 0):
+            plt.imshow(np.sqrt(ux ** 2 + uy ** 2))
             plt.pause(0.01)
             plt.cla()
-
 
 if __name__ == "__main__":
     main()
