4d20f4209ff045b4407608938d2b28cc81c5db51
tebuRRubet <rubet.jurgens@student.uva.nl>
2025-01-25 00:10:45 +0100

    Update lbm.py

--- lbm.py
+++ lbm.py
@@ -1,59 +1,74 @@
-import os
-import numpy as np
+import taichi as ti
+import taichi.math as tm
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import numpy as np
+
+ti.init(arch=ti.gpu)
 
 
-def distance(x1, y1, x2, y2):
-    return np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+# grid = np.arange(50 * 50 * 9).reshape(50, 50, 9)
+grid = np.random.randn(150, 150, 9)
+# grid = np.ones((5, 5, 9))
+
+coords = [(-1, 1), (0, 1), (1, 1), (-1, 0), (0, 0), (1, 0), (-1, -1), (0, -1),
+          (1, -1)]
 
 
-def main():
-    plot_every = 1
-    Nx, Ny = 400, 100
-    tau = 0.53
-    Nt = 3000
+def streaming(vals):
+    for i, (x, y) in enumerate(coords):
+        vals[:, :, i] = np.roll(vals[:, :, i], x, axis=1)
+        vals[:, :, i] = np.roll(vals[:, :, i], -y, axis=0)
+    return vals
 
-    NL = 9
-    cxs = np.array([0, 0, 1, 1, 1, 0, -1, -1, -1])
-    cys = np.array([0, 1, 1, 0, -1, -1, -1, 0, 1])
-    weights = np.array([4/9, 1/9, 1/36, 1/9, 1/36, 1/9, 1/36, 1/9, 1/36])
 
-    F = np.ones((Ny, Nx, NL)) + 0.01 * np.random.randn(Ny, Nx, NL)
-    F[:, :, 3] = 2.3
 
-    cylinder = np.full((Ny, Nx), False)
-    for y in range(Ny):
-        for x in range(Nx):
-            if distance(Nx / 4, Ny / 2, x, y) < 13:
-                cylinder[y][x] = True
+def collision(vals, tau=5):
+    # i = 0
+    rho = np.sum(vals, axis=2)
+    w = np.array([1, 4, 1, 4, 16, 4, 1, 4, 1]) / 9
+    u = np.array([np.array([sum([vals[i, j][k] * np.array([x, y]) for k, (x, y) in enumerate(coords)]) for j in range(vals.shape[1])]) for i in range(vals.shape[0])]) / rho[:, :, None]
+    feq = np.zeros_like(vals)
+    for i in range(vals.shape[0]):
+        for j in range(vals.shape[1]):
+            for k in range(9):
+                val = w[k] * rho[i, j] * (1 + 3 * np.array(coords[k]).dot(u[i, j]) + 9 / 2 * np.array(coords[k]).dot(u[i, j]) ** 2 - 3 / 2 * u[i, j].dot(u[i, j]))
+                feq[i, j][k] = val
+    print(feq)
+    return vals + (feq - vals) / tau
 
-    for it in range(Nt):
-        print(it)
-        for i, cx, cy in zip(range(NL), cxs, cys):
-            F[:, :, i] = np.roll(F[:, :, i], cx, axis=1)
-            F[:, :, i] = np.roll(F[:, :, i], cy, axis=0)
-        bndryF = F[cylinder, :]
-        bndryF = bndryF[:, [0, 5, 6, 7, 8, 1, 2, 3, 4]]
 
-        rho = np.sum(F, 2)
-        ux = np.sum(F * cxs, 2) / rho
-        uy = np.sum(F * cys, 2) / rho
 
-        F[cylinder, :] = bndryF
-        ux[cylinder] = 0
-        uy[cylinder] = 0
 
-        Feq = np.zeros(F.shape)
-        for i, cx, cy, w in zip(range(NL), cxs, cys, weights):
-            Feq[:, :, i] = rho * w * (
-                1 + 3 * (cx * ux + cy * uy) + 9 * (cx * ux + cy * uy) ** 2 / 2 - 3 * (ux ** 2 + uy ** 2) / 2
-            )
-            F = F + -(1/tau) * (F - Feq)
+fig, ax = plt.subplots(figsize=(8, 6))
 
-        if (it % plot_every == 0):
-            plt.imshow(np.sqrt(ux ** 2 + uy ** 2))
-            plt.pause(0.01)
-            plt.cla()
+def update(iteration):
+    global grid  # Use the global grid variable
+    grid = collision(streaming(grid))  # Perform one iteration of streaming + collision
 
-if __name__ == "__main__":
-    main()
+    # Compute density and velocity
+    rho = np.sum(grid, axis=2)
+    rho[rho == 0] = 1  # Prevent division by zero
+    coords_array = np.array(coords)
+    u = np.tensordot(grid, coords_array, axes=(2, 0)) / rho[:, :, None]
+
+    # Clear and redraw the plot
+    ax.clear()
+    ax.imshow(rho, cmap='viridis', origin='lower')
+    # ax.quiver(
+    #     np.arange(grid.shape[1]),
+    #     np.arange(grid.shape[0]),
+    #     u[:, :, 0],
+    #     u[:, :, 1],
+    #     scale=1,
+    #     scale_units='xy',
+    #     color='white',
+    # )
+    ax.set_title(f"Iteration {iteration}")
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+
+# Create animation
+ani = FuncAnimation(fig, update, frames=100, interval=100)  # 100 frames, 100ms delay
+plt.show()
