6d9bb3e87a939ff890b2ab3c838f12d85defba78
tebuRRubet <rubet.jurgens@student.uva.nl>
2025-01-25 01:46:23 +0100

    Update lbm.py
    Vectorised to be faster. Taichi implementation will come next

--- lbm.py
+++ lbm.py
@@ -10,31 +10,20 @@ n = 150
 
 grid = np.arange(n * n * 9).reshape(n, n, 9)
 grid = np.random.randn(n, n, 9)
-# grid = np.ones((n, n, 9))
-# grid[:, :, 5] = 2
+
 
 coords = [(-1, 1), (0, 1), (1, 1), (-1, 0), (0, 0), (1, 0), (-1, -1), (0, -1),
           (1, -1)]
 coords = [np.array(c) for c in coords]
 
-
-
-
-
-
-
-n = 50
+n = 100
 
 rho_init = 1.0
 f_init = rho_init / 9
 grid = np.full((n, n, 9), f_init)
 
-grid[n//2, 0, 5] += 0.3
+grid[n//2, 0, 5] += 1
 grid[n//2 + 10, -1, 3] += 0.2
-
-
-
-
 
 
 def streaming(vals):
@@ -47,21 +36,28 @@ def streaming(vals):
 def collision(vals, tau=5):
     rho = np.sum(vals, axis=2)
     w = np.array([1, 4, 1, 4, 16, 4, 1, 4, 1]) / 9
-    u = np.zeros((n, n, 2))
-    for y in range(n):
-        for x in range(n):
-            for i in range(9):
-                u[y, x] += vals[y, x, i] * coords[i]
-            if rho[y, x] > 0:
-                u[y, x] /= rho[y, x]
-            else:
-                u[y, x] = np.array([0, 0])
-    feq = np.zeros_like(grid)
-    for y in range(n):
-        for x in range(n):
-            uu = u[y, x]
-            for i in range(9):
-                feq[y, x][i] = w[i] * rho[y, x] * (1 + 3 * np.dot(coords[i], uu) + 9/2 * np.dot(coords[i], uu) ** 2 - 3/2 * (np.dot(uu, uu)))
+    u = np.tensordot(vals, coords, axes=([2], [0]))
+    u = np.divide(u, rho[:, :, None], where=rho[:, :, None] != 0, out=np.zeros_like(u))
+    # for y in range(n):
+    #     for x in range(n):
+    #         u[y, x] = np.dot(vals)
+    #         for i in range(9):
+    #             u[y, x] += vals[y, x, i] * coords[i]
+    #         if rho[y, x] > 0:
+    #             u[y, x] /= rho[y, x]
+    #         else:
+    #             u[y, x] = np.array([0, 0])
+    uu = np.sum(u ** 2, axis=-1)
+    cu = np.einsum('qd,abd->abq', coords, u)
+    feq = w[None, None, :] * rho[:, :, None] * (
+        1 + 3 * cu + 9/2 * cu ** 2 - 3/2 * uu[:, :, None]
+    )
+    # feq = np.zeros_like(grid)
+    # for y in range(n):
+    #     for x in range(n):
+    #         uu = u[y, x]
+    #         for i in range(9):
+    #             feq[y, x][i] = w[i] * rho[y, x] * (1 + 3 * np.dot(coords[i], uu) + 9/2 * np.dot(coords[i], uu) ** 2 - 3/2 * (np.dot(uu, uu)))
     return vals + (feq - vals) / tau
 
 
@@ -71,7 +67,6 @@ fig, ax = plt.subplots(figsize=(8, 6))
 def update(iteration):
     global grid
     grid = collision(streaming(grid))
-
 
     rho = np.sum(grid, axis=2)
     rho[rho == 0] = 1
@@ -84,5 +79,5 @@ def update(iteration):
     ax.set_ylabel("y")
 
 
-ani = FuncAnimation(fig, update, frames=100, interval=100)
+ani = FuncAnimation(fig, update, frames=100, interval=10)
 plt.show()
