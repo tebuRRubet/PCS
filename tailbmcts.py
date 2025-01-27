2d7389472d68a703fc56fbf769ba9bb9505c66e0
tebuRRubet <rubet.jurgens@student.uva.nl>
2025-01-28 00:48:24 +0100

    Simplified collide and stream
    Updated the rest of the ("non-basic") functions to reflect this code simplification (there were some semi-significant changes to the initial setup). The boundary reflection works with the new implementation. The "basic" functions throw an indexing-related error I was not able to figure out.
    
    Also updated the vector-reversing method.
    
    Also cleaned the code up a little bit (names, syntax, simplifications).
    
    The simulation is stable for the first few seconds after which it breaks down due to significant jumps in velocity which prompt priority investigation.
    
    Performance is also down 20% on my end, but I was unable to find the reason for this.

--- tailbmcts.py
+++ tailbmcts.py
@@ -15,28 +15,22 @@ def precompute_colormap():
     return colors.astype(np.float32)
 
 
-
-
 @ti.data_oriented
 class LBM:
     def __init__(self, n=1000, tau=5):
         tau = 15
-        coords = [(-1, 1), (0, 1), (1, 1),
-                  (-1, 0), (0, 0), (1, 0),
-                  (-1, -1), (0, -1), (1, -1)]
-        self.coords = ti.Vector.field(n=2, dtype=ti.i32, shape=(9,))
-        for i in range(9):
-            self.coords[i] = coords[i]
+        self.dirs = ti.Matrix([(-1, 1), (0, 1), (1, 1),
+                               (-1, 0), (0, 0), (1, 0),
+                               (-1, -1), (0, -1), (1, -1)])
         self.n = n
         self.disp = ti.field(dtype=ti.f32, shape=(n, n))
-        self.grid = ti.Vector.field(n=9, dtype=ti.f32, shape=(n, n))
-        self.update_grid = ti.Vector.field(n=9, dtype=ti.f32, shape=(n, n))
-        self.tau = tau
+        self.f1 = ti.Vector.field(n=9, dtype=ti.f32, shape=(n, n))
+        self.f2 = ti.Vector.field(n=9, dtype=ti.f32, shape=(n, n))
         self.tau_inv = 1 / tau
-        self.u = ti.field(dtype=ti.f32, shape=(n, n))
+        self.vel = ti.field(dtype=ti.f32, shape=(n, n))
         self.w = ti.field(dtype=ti.f32, shape=(9,))
-        w = np.array([1, 4, 1, 4, 16, 4, 1, 4, 1]) / 36
-        self.w.from_numpy(w)
+        self.w.from_numpy(np.array([1, 4, 1, 4, 16, 4, 1, 4, 1]) / 36)
+
         self.colormap = ti.Vector.field(3, dtype=ti.f32, shape=(256,))
         colors = precompute_colormap()
         self.colormap.from_numpy(colors)
@@ -51,18 +45,17 @@ class LBM:
         for i in (0,):
             for j in range(self.n):
                 self.boundary[i, j] = 1
-                self.grid[i, j].fill(0)
+                self.f1[i, j].fill(0)
 
         for j in (0, self.n - 1):
             for i in range(self.n):
                 self.boundary[i, j] = 1
-                self.grid[i, j].fill(0)
+                self.f1[i, j].fill(0)
 
-        for i in range(450- 300, 550-300):
+        for i in range(450 - 300, 550-300):
             for j in range(450, 550):
                 self.boundary[i, j] = 1
-                self.grid[i, j].fill(0)
-
+                self.f1[i, j].fill(0)
 
         # for i in range(-10, 11):
         #     for j in range(-10, 11):
@@ -75,80 +68,75 @@ class LBM:
         #         if i**2 + j ** 2 < 200:
         #             self.grid[n//2 + j, n//2 + i + 200][1] += 1
 
-        self.grid.fill(1/9)
-        self.grid.fill(0)
+        self.f1.fill(1/9)
+        self.f1.fill(0)
         self.init_grid()
-        # for i in range(-10, 11):
-        #     for j in range(-10, 11):
-        #         if i**2 + j ** 2 < 200:
-        #             for k in range(1):
-        #                 self.grid[-100 + n//2 + j, n//2 + i][5] += w[k]
+        for i in range(-10, 11):
+            for j in range(-10, 11):
+                if i**2 + j ** 2 < 200:
+                    for k in range(1):
+                        self.f1[-100 + n//2 + j, n//2 + i][5] += self.w[k]
         print("Init done")
-
 
     @ti.kernel
     def init_grid(self):
-        for i, j in self.grid:
+        for i, j in self.f1:
             for k in ti.static(range(9)):
-                self.grid[i, j][k] = self.w[k]
-                # print(self.w[k])
+                self.f1[i, j][k] = self.w[k]
 
     @ti.kernel
     def stream(self):
         for i, j in ti.ndrange((1, self.n - 1), (1, self.n - 1)):
             for k in ti.static(range(9)):
-                self.update_grid[i + self.coords[k].x, j + self.coords[k].y][k] = self.grid[i, j][k]
+                self.f2[i + self.dirs[k, 0], j + self.dirs[k, 1]][k] = self.f1[i, j][k]
 
     @ti.kernel
     def collide_and_stream(self):
         for i, j in ti.ndrange(ti.static((1, self.n - 1)), ti.static((1, self.n - 1))):
-            rho = self.grid[i, j].sum()
-            u = ti.Vector([0.0, 0.0])
+            rho = self.f1[i, j].sum()
+            # Calculates velocity vector in one step
+            vel = (self.dirs.transpose() @ self.f1[i, j] / rho) if rho > 0 else tm.vec2([0, 0])
+            self.vel[i, j] = vel.norm()
             for k in ti.static(range(9)):
-                u += self.coords[k] * self.grid[i, j][k]
-
-            u = (u / rho) if rho > 0 else tm.vec2([0, 0])
-            self.u[i, j] = u.norm()
-            # print("Done")
-            for k in ti.static(range(9)):
-                feq = self.w[k] * rho * (1 + 3 * tm.dot(self.coords[k], u) + 4.5 * tm.dot(self.coords[k], u) ** 2 - 1.5 * tm.dot(u, u))
-                # if self.n > i + self.coords[k].x >= 0 and self.n > j + self.coords[k].y >= 0:
-                self.update_grid[i + self.coords[k].x, j + self.coords[k].y][k] = (1 - self.tau_inv) * self.grid[i, j][k] + self.tau_inv * feq
-
-
+                cm = vel[0] * self.dirs[k, 0] + vel[1] * self.dirs[k, 1]
+                feq = self.w[k] * rho * (1 + 3 * cm + 4.5 * cm ** 2 - 1.5 * tm.dot(vel, vel))
+                self.f2[i + self.dirs[k, 0], j + self.dirs[k, 1]][k] = (1 - self.tau_inv) * self.f1[i, j][k] + self.tau_inv * feq
 
     @ti.func
     def total_density(self):
         dens = 0.0
-        for i, j in self.grid:
-            ti.atomic_add(dens, self.grid[i, j].sum())
+        for i, j in self.f1:
+            ti.atomic_add(dens, self.f1[i, j].sum())
         return dens
 
     @ti.func
     def reverse_vector(self, x):
-        return ti.Vector([x[i] for i in range(x.get_shape()[0])][::-1])
+        return ti.Vector([x[8 - i] for i in ti.static(range(9))])
 
     @ti.kernel
     def bounce_boundary(self):
-        for i, j in self.grid:
+        for i, j in self.f1:
             if self.boundary[i, j]:
                 if i == 0:
                     # self.grid[i, j].fill(0)
-                    self.update_grid[i, j][5] = 1
+                    self.f2[i, j][5] = 1
+                if i == self.n - 1:
+                    # self.update_grid[i, j] = ti.Vector([0] * 9)
+                    self.vel[i, j] = 0
                 for k in ti.static(range(9)):
-                    self.grid[i + self.coords[k].x, j + self.coords[k].y][8-k] = self.update_grid[i, j][k]
+                    self.f1[i + self.dirs[k, 0], j + self.dirs[k, 1]] = self.reverse_vector(self.f2[i, j])
                 # self.gird[i, j].fill(0)
                 # self.grid[i, j] = self.reverse_vector(self.update_grid[i, j])
                 # self.u[i, j] = tm.vec2([0, 0])
             else:
-                self.grid[i, j] = self.update_grid[i, j]
+                self.f1[i, j] = self.f2[i, j]
 
     """Check performance"""
     @ti.kernel
     def update(self):
         # self.grid.copy_from(self.update_grid)
-        for i, j in self.update_grid:
-            self.grid[i, j] = self.update_grid[i, j]
+        for i, j in self.f2:
+            self.f1[i, j] = self.f2[i, j]
 
     def apply_colormap(self, data):
         norm_data = (data - data.min()) / (data.max() - data.min() + 1e-8)
@@ -158,16 +146,16 @@ class LBM:
     @ti.kernel
     def normalize_and_map(self):
         # print(self.total_density())
-        for i, j in self.u:
-            self.max_val[None] = ti.max(self.max_val[None], self.u[i, j])
+        for i, j in self.vel:
+            self.max_val[None] = ti.max(self.max_val[None], self.vel[i, j])
         curr_max = 1e-8
-        for i, j in self.u:
-            curr_max = ti.max(curr_max, self.u[i, j])
-        # print(self.max_val[None], curr_max)
+        for i, j in self.vel:
+            curr_max = ti.max(curr_max, self.vel[i, j])
+        print(self.max_val[None], curr_max)
 
-        for i, j in self.u:
-            norm_val = ti.cast(self.u[i, j] / self.max_val[None] * (255), ti.i32)
-            norm_val = ti.cast(self.u[i, j] / curr_max * (255), ti.i32)
+        for i, j in self.vel:
+            norm_val = ti.cast(self.vel[i, j] / self.max_val[None] * (255), ti.i32)
+            # norm_val = ti.cast(self.u[i, j] / curr_max * (255), ti.i32)
 
             norm_val = ti.min(ti.max(norm_val, 0), (255))
             for c in ti.ndrange(3):
@@ -175,50 +163,47 @@ class LBM:
                 if self.boundary[i, j]:
                     self.rgb_image[i, j][c] = (255)
 
-
-
-
     @ti.kernel
-    def stream_new(self):
-        for i, j in self.grid:
+    def stream_basic(self):
+        for i, j in self.f1:
             for k in ti.static(range(9)):
-                ni, nj = i + self.coords[k].x, j + self.coords[k].y
+                ni, nj = i + self.dirs[k, 0], j + self.dirs[k, 1]
                 if 0 <= ni < self.n and 0 <= nj < self.n:
-                    self.update_grid[ni, nj][k] = self.grid[i, j][k]
-
+                    self.f2[ni, nj][k] = self.f1[i, j][k]
 
     @ti.kernel
-    def collide_new(self):
-        for i, j in self.update_grid:
-            rho = self.update_grid[i, j].sum()
+    def collide_basic(self):
+        # What??..........
+        for i, j in ti.ndrange((0, self.n), (0, self.n)):
+            rho = self.f2[i, j].sum()
             u = tm.vec2([0.0, 0.0])
-            for k in range(9):
-                u += self.coords[k] * self.update_grid[i, j][k]
+            for k in ti.static(range(9)):
+                # Compiler throws error on self.dirs[k, 0] indexing. Not sure why.
+                u += self.f2[i, j][k] * tm.vec2([self.dirs[k, 0], self.dirs[k, 1]])
             if rho != 0:
                 if rho < 0:
                     print("WHAAAT THE FUUUCKKKK")
                 u /= rho
             else:
                 u = tm.vec2([0, 0])
-            self.u[i, j] = u
+            self.vel[i, j] = u
             for k in range(9):
-                feq = self.w[k] * rho * (1 + 3 * tm.dot(self.coords[k], u) + 4.5 * tm.dot(self.coords[k], u) ** 2 - 1.5 * tm.dot(u, u))
-                self.update_grid[i, j][k] += self.tau_inv * (feq - self.update_grid[i, j][k])
-
+                cm = u[0] * self.dirs[k, 0] + u[1] * self.dirs[k, 1]
+                feq = self.w[k] * rho * (1 + 3 * cm + 4.5 * cm ** 2 - 1.5 * tm.dot(u, u))
+                self.f2[i, j][k] += self.tau_inv * (feq - self.f2[i, j][k])
 
     @ti.kernel
     def display_new(self):
-        for i, j in self.grid:
-            self.disp[i, j] = self.grid[i, j].sum() / 9
-
+        for i, j in self.f1:
+            self.disp[i, j] = self.f1[i, j].sum() / 9
 
     def display(self):
         gui = ti.GUI('LBM Simulation', (self.n, self.n))
-        self.update_grid.copy_from(self.grid)
+        self.f2.copy_from(self.f1)
         self.stream()
         while not gui.get_event(ti.GUI.ESCAPE, ti.GUI.EXIT):
-            # self.stream_new()
-            # self.collide_new()
+            # self.stream_basic()
+            # self.collide_basic()
             # self.update()
             # self.bounce_boundary()
             # self.get_velocity_magnitude()
@@ -226,13 +211,17 @@ class LBM:
             # gui.set_image(self.rgb_image)
             # gui.show()
 
-
             self.normalize_and_map()
             gui.set_image(self.rgb_image)
             gui.show()
-            self.collide_and_stream()
-            # self.update()
-            self.bounce_boundary()
+            for _ in range(10):
+                self.collide_and_stream()
+                # self.stream()
+                # self.update()
+                # print(self.dirs[0])
+
+                self.bounce_boundary()
+                # exit()
 
 
 L = LBM()
