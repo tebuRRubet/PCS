b69391216d3de9c0b2296c3770a208dd37f33e47
Naushaadw <naushaadwazir@gmail.com>
2025-01-29 16:23:05 +0100

    Obstacle transform functions update

--- obstacles.py
+++ obstacles.py
@@ -4,9 +4,7 @@ CYLINDER, EGG, AIRFOIL = 0, 1, 2
 
 
 @ti.func
-def translate_scale_rotate(i, j, di, dj, scale, theta):
-    x, y = (i - di) / scale, (j - dj) / scale
-
+def rotate(x, y, theta):
     theta = ti.cast(theta * ti.math.pi / 180.0, ti.f32)
     cos_theta = ti.cos(theta)
     sin_theta = ti.sin(theta)
@@ -19,20 +17,9 @@ def distance(x1, y1, x2, y2):
 
 
 @ti.func
-def glt(x, y, cylinder_r, a, b):
-    return (x / cylinder_r) + a, \
-        (y / cylinder_r) + b
-
-
-@ti.func
-def inverse_glt(x, y, cylinder_r, a, b):
-    return cylinder_r * (x - a), cylinder_r * (y - b)
-
-
-@ti.func
-def joukowski_transform(self, x, y):
-    r = x**2 + y**2
-    return x * (1 + 1 / r), y * (1 - 1 / r)
+def glt(x, y, horizontal_shift, vertical_shift, scale, new_x, new_y, new_r):
+    return ((x - horizontal_shift) / scale) * new_r + new_x, \
+        ((y - vertical_shift) / scale) * new_r + new_y
 
 
 @ti.func
@@ -50,36 +37,36 @@ def inverse_joukowski_transform(alpha, beta):
 
 
 @ti.func
-def is_in_cylinder(x, y, cylinder_r):
-    return ti.cast(distance(x, y, 0, 0) <= cylinder_r, ti.i32)
+def is_in_cylinder(x, y, scale):
+    return ti.cast(distance(x, y, 0, 0) <= scale, ti.i32)
 
 
 @ti.func
-def is_in_egg(x, y, cylinder_r):
+def is_in_egg(x, y, scale):
     x_shifted, y_shifted = x, y
     r_squared = x_shifted**2 + y_shifted**2
     discriminant = ti.sqrt(ti.abs(r_squared - 4.0))
     zeta_x = (x_shifted - discriminant) * 0.5
     zeta_y = (y_shifted - discriminant) * 0.5
-    return ti.cast(zeta_x**2 + zeta_y**2 <= cylinder_r**2, ti.i32)
+    return ti.cast(zeta_x**2 + zeta_y**2 <= scale**2, ti.i32)
 
 
 @ti.func
-def is_in_airfoil(alpha, beta, cylinder_r, a, b):
-    alpha2, beta2 = glt(alpha, beta, cylinder_r, a, b)
+def is_in_airfoil(alpha, beta, horizontal_shift, vertical_shift, scale, new_x, new_y, new_r):
+    alpha2, beta2 = glt(alpha, beta, horizontal_shift, vertical_shift, scale, new_x, new_y, new_r)
     x1, y1, x2, y2 = inverse_joukowski_transform(alpha2, beta2)
-    check1 = distance(x1, y1, a, b) <= 1
-    check2 = distance(x2, y2, a, b) <= 1
+    check1 = distance(x1, y1, new_x, new_y) <= new_r
+    check2 = distance(x2, y2, new_x, new_y) <= new_r
     return ti.cast(not (check1 or check2), ti.i32)
 
 
 @ti.func
-def is_in_obstacle(x, y, obstacle, cylinder_r, a, b):
+def is_in_obstacle(x, y, obstacle, horizontal_shift, vertical_shift, scale, new_x, new_y, new_r):
     result = 0
     if obstacle == CYLINDER:
-        result = is_in_cylinder(x, y, cylinder_r)
+        result = is_in_cylinder(x, y, scale)
     elif obstacle == EGG:
-        result = is_in_egg(x, y, cylinder_r)
+        result = is_in_egg(x, y, scale)
     elif obstacle == AIRFOIL:
-        result = is_in_airfoil(x, y, cylinder_r, a, b)
+        result = is_in_airfoil(x, y, horizontal_shift, vertical_shift, scale, new_x, new_y, new_r)
     return result
