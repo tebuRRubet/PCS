1371f4068d5bdcec530c409a09038b953d85c4d9
tebuRRubet <rubet.jurgens@student.uva.nl>
2025-01-30 16:04:47 +0100

    Set initial conditions more in-line with physical values and improved inlet
    Zou-he may need more improving depending on stability tests

--- lbm.py
+++ lbm.py
@@ -10,10 +10,11 @@ from obstacles import rotate, is_in_obstacle
 ti.init(arch=ti.gpu)
 
 CYLINDER, EGG, AIRFOIL = 0, 1, 2
+UP_LEFT, UP, UP_RIGHT, LEFT, MID, RIGHT, DOWN_LEFT, DOWN, DOWN_RIGHT = range(9)
 
 
 def precompute_colormap():
-    viridis = color_m.get_cmap('plasma', 256)
+    viridis = color_m.get_cmap('viridis', 256)
     # Extract RGB values
     colors = viridis(np.linspace(0, 1, 256))[:, :3]
     return colors.astype(np.float32)
@@ -21,7 +22,7 @@ def precompute_colormap():
 
 @ti.data_oriented
 class LBM:
-    def __init__(self, width=1024, height=512, tau=0.55, rho0=1.0, inlet_val=0.15, block_size=128):
+    def __init__(self, width=1024, height=512, tau=0.8, rho0=1.0, inlet_val=0.2, block_size=128):
         if width % block_size or height % block_size:
             print(f"Error, block_size ({block_size}) must be a divisor of n ({width}) and m ({height})!")
             print(f"{width} = {width // block_size} * {block_size} + {width % block_size}.")
@@ -54,18 +55,14 @@ class LBM:
         self.rgb_image = ti.Vector.field(3, dtype=ti.u8, shape=(width, height))
         self.max_val = ti.field(ti.f32, shape=())
         self.max_val.fill(1e-8)
+        self.min_val = ti.field(ti.f32, shape=())
+        self.min_val.fill(1e-8)
         self.inlet_val = inlet_val
-
-
 
         self.temp_max = ti.field(ti.f32, shape=())
         self.temp_max.fill(1e-8)
         self.cont = ti.field(ti.i8, shape=())
         self.cont.fill(1)
-
-
-
-
 
         self.boundary_mask = ti.field(ti.i8)
         self.b_sparse_mask = ti.root.pointer(ti.ij, (width // block_size, height // block_size))
@@ -78,12 +75,15 @@ class LBM:
         a = 0.026
         b = 0.077
         r = 0.918
-        # scale = width//20
-        # a = 0
-        # b = 0
-        # r = 1
-        theta = 0
+        theta = 5
         self.init_grid(rho0, obstacle, center_x, center_y, scale, a, b, r, theta)
+        obstacle = CYLINDER
+        scale = width//20
+        a = 0
+        b = 0
+        r = 1
+        theta = 0
+        # self.init_grid(rho0, obstacle, center_x - 400, center_y, scale, a, b, r, theta)
 
     @ti.func
     def feq(self, weight, rho, cm, vel):
@@ -96,7 +96,6 @@ class LBM:
             vel = (self.dirs @ self.f1[i, j] / rho0) if rho0 > 0 else tm.vec2([0, 0])
             self.vel[i, j] = vel.norm()
             di, dj = rotate(i, j, center_x, center_y, theta)
-
             if is_in_obstacle(di, dj, obstacle, center_x, center_y, scale, a, b, r):
                 self.boundary_mask[i, j] = 1
 
@@ -104,10 +103,15 @@ class LBM:
                 cm = vel[0] * self.dirs[0, k] + vel[1] * self.dirs[1, k]
                 self.f1[i, j][k] = self.feq(self.w[k], rho0, cm, tm.dot(vel, vel))
 
+            # if j == 0 or j == self.height - 1:
+            #     if j == self.height - 1 and i == 300:
+            #         print("Marked")
+            #     self.boundary_mask[i, j] = 1
+
     @ti.kernel
     def normalize_and_map(self):
         for i, j in self.rgb_image:
-            norm_val = ti.cast(self.vel[i, j] / self.max_val[None] * (255), ti.i32)
+            norm_val = ti.cast((self.vel[i, j] - self.min_val[None]) / (self.max_val[None] - self.min_val[None]) * (255), ti.i32)
 
             norm_val = ti.min(ti.max(norm_val, 0), (255))
             for c in ti.static(range(3)):
@@ -117,8 +121,15 @@ class LBM:
     @ti.func
     def draw_boundary(self):
         for i, j in self.boundary_mask:
+            # if i == 300 and j > self.width - 100:
+            #     print(i, j)
             for c in ti.static(range(3)):
                 self.rgb_image[i, j][c] = 255
+        # for i in ti.ndrange(self.width):
+        #     for c in ti.static(range(3)):
+        #         self.rgb_image[i, 0][c] = 255
+        #         self.rgb_image[i, self.height - 1][c] = 255
+
 
     @ti.kernel
     def stream(self):
@@ -130,22 +141,37 @@ class LBM:
     def collide_and_stream(self):
         for i, j in ti.ndrange(ti.static((1, self.width - 1)), ti.static((1, self.height - 1))):
             rho = self.f1[i, j].sum()
-            # Calculates velocity vector in one step
             vel = self.dirs @ self.f1[i, j] / rho
             self.vel[i, j] = vel.norm()
             for k in ti.static(range(9)):
                 cm = vel[0] * self.dirs[0, k] + vel[1] * self.dirs[1, k]
                 self.f2[i + self.dirs[0, k], j + self.dirs[1, k]][k] = (1 - self.tau_inv) * self.f1[i, j][k] + self.tau_inv * self.feq(self.w[k], rho, cm, tm.dot(vel, vel))
 
+    # @ti.kernel
+    # def apply_inlet(self, t: ti.types.f32, T: ti.types.i32):
+    #     xv = self.inlet_val * (1 - ti.exp(-t / T))
+    #     for i in ti.ndrange((1, self.height - 1)):
+    #         rho = self.f1[1, i].sum()
+    #         vel = self.dirs @ self.f1[0, i] / rho
+    #         for k in ti.static((2, 5, 8)):
+    #             cm = vel[0] * self.dirs[0, k] + vel[1] * self.dirs[1, k]
+    #             self.f1[1, i][k] = self.feq(self.w[k], rho / (1 - xv), cm, 0.01)
+
+
     @ti.kernel
     def apply_inlet(self, t: ti.types.f32, T: ti.types.i32):
-        xv = self.inlet_val * (1 - ti.exp(-t / T))
-        for i in ti.ndrange(self.height):
-            rho = self.f1[1, i].sum()
-            vel = self.dirs @ self.f1[0, i] / rho
-            for k in ti.static((2, 5, 8)):
-                cm = vel[0] * self.dirs[0, k] + vel[1] * self.dirs[1, k]
-                self.f1[1, i][k] = self.feq(self.w[k], rho / (1 - xv), cm, 0.01)
+        xv = self.inlet_val * (1 - ti.exp(-t / T))  # Inlet velocity
+        for i in ti.ndrange((1, self.height - 1)):
+            rho = self.f2[1, i].sum() / (1 - xv)
+            # Compute density using Zou-He formulation
+            # rho = (self.f1[1, i][0] + self.f1[1, i][2] + self.f1[1, i][4] +
+                # 2 * (self.f1[1, i][3] + self.f1[1, i][6] + self.f1[1, i][7])) / (1 - xv)
+
+            # Zou-He updates for missing right-moving distributions
+            self.f2[1, i][RIGHT] = self.f2[1, i][LEFT] + (2 / 3) * rho * xv
+            self.f2[1, i][UP_RIGHT] = self.f2[1, i][UP_LEFT] + (1 / 6) * rho * xv
+            self.f2[1, i][DOWN_RIGHT] = self.f2[1, i][DOWN_LEFT] + (1 / 6) * rho * xv
+
 
     @ti.kernel
     def apply_outlet(self):
@@ -159,6 +185,19 @@ class LBM:
             for k in ti.static(range(9)):
                 self.f2[i + self.dirs[0, 8 - k], j + self.dirs[1, 8 - k]][8 - k] = self.f2[i, j][k]
 
+    @ti.kernel
+    def upper_bound(self):
+        for i in ti.ndrange((1, self.width - 1)):
+            self.f2[i + self.dirs[0, DOWN_RIGHT], self.height - 1 + self.dirs[1, DOWN_RIGHT]][DOWN_RIGHT] = self.f2[i, self.height - 1][UP_LEFT]
+            self.f2[i + self.dirs[0, DOWN], self.height - 1 + self.dirs[1, DOWN]][DOWN] = self.f2[i, self.height - 1][UP]
+            self.f2[i + self.dirs[0, DOWN_LEFT], self.height - 1 + self.dirs[1, DOWN_LEFT]][DOWN_LEFT] = self.f2[i, self.height - 1][UP_RIGHT]
+
+            self.f2[i + self.dirs[0, UP_RIGHT], self.dirs[1, UP_RIGHT]][UP_RIGHT] = self.f2[i, 0][DOWN_LEFT]
+            self.f2[i + self.dirs[0, UP], self.dirs[1, UP]][UP] = self.f2[i, 0][DOWN]
+            self.f2[i + self.dirs[0, UP_LEFT], self.dirs[1, UP_LEFT]][UP_LEFT] = self.f2[i, 0][DOWN_RIGHT]
+
+
+
     """Check performance"""
     @ti.kernel
     def update(self):
@@ -169,9 +208,14 @@ class LBM:
     @ti.kernel
     def max_vel(self):
         curr_max = 1e-8
-        for i, j in self.vel:
+        curr_min = float('inf')
+        for i, j in ti.ndrange((1, self.width - 1), (1, self.height - 1)):
             ti.atomic_max(curr_max, self.vel[i, j])
+            ti.atomic_min(curr_min, self.vel[i, j])
+
         self.max_val[None] = self.max_val[None] * 0.9 + curr_max * 0.1
+        self.min_val[None] = self.min_val[None] * 0.9 + curr_min * 0.1
+
 
     @ti.kernel
     def upper_sum(self):
@@ -209,7 +253,7 @@ class LBM:
         output_folder = "lbm_frames"
         os.makedirs(output_folder, exist_ok=True)
 
-        # frame_count = 0
+        frame_count = 0
         step = 0
         max_step = 5000
         self.f2.copy_from(self.f1)
@@ -223,15 +267,18 @@ class LBM:
             # filename = os.path.join(output_folder, f"frame_{frame_count:04d}.png")
             # gui.show(filename)
             gui.show()
-            # frame_count += 1
+            frame_count += 1
             # print(frame_count)
+            if step == max_step:
+                print("Inlet max")
 
-            for _ in range(100):
+            for _ in range(10):
                 step += 1
                 self.apply_inlet(step, max_step)
                 self.apply_outlet()
                 self.collide_and_stream()
                 self.boundary_condition()
+                # self.upper_bound()
                 self.update()
 
 
@@ -251,7 +298,7 @@ class LBM:
             # self.lower_sum()
 
         # print("Simulation ended. Generating GIF...")
-        # subprocess.run(["python", "generate_gif.py"])
+        # subprocess.run(["python3", "generate_gif.py"])
 
 from time import sleep
 L = LBM()
